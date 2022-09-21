3d07cb070a7354cabe9ddf2a5ec6d63f6cef0918
Ernest Ajieh <ajieh.c.ernest@gmail.com>
2022-09-20 22:25:57 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,22 +1,14 @@
 i#include <stdio.h>
-
 #include "main.h"
 
 
-
 /**
+ *_puts - prints a string
+ * @str: string to print
  *
- *  *_puts - prints a string
- *
- *   * @str: string to print
- *
- *    *
- *
- *     * Description: prints a string
- *
- *      * On success: returns no error
- *
- *       */
+ * Description: prints a string
+ * On success: returns no error
+ */
 
 
 
