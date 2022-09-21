310968520ec96c3a1fd1b6f2cf0d6a6e7e5a2cb9
Ernest Ajieh <ajieh.c.ernest@gmail.com>
2022-09-20 22:53:41 -0700

    task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,11 @@
 #ifndef MAIN_H
-
 #define MAIN_H
 
-int _putchar(char c);
+/**
+ * void prototypes()
+ * int prototypes()
+ */
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -13,5 +16,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *str);
 
-#endif
+#endif /*MAIN_H*/
